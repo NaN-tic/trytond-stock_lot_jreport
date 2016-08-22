5b505e08b1e0afd0b603aceac24435c4c28d2928
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:52:01 +0200

    Upgrade test to version 4.0

--- tests/test_stock_lot_jreport.py
+++ tests/test_stock_lot_jreport.py
@@ -2,63 +2,48 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import datetime
 from dateutil.relativedelta import relativedelta
 from decimal import Decimal
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
-from trytond.transaction import Transaction
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+
+from trytond.modules.company.tests import create_company, set_company
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
+    module = 'stock_lot_jreport'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_lot_jreport')
-        self.company = POOL.get('company.company')
-        self.user = POOL.get('res.user')
-        self.stock_configuration = POOL.get('stock.configuration')
-        self.uom = POOL.get('product.uom')
-        self.template = POOL.get('product.template')
-        self.product = POOL.get('product.product')
-        self.location = POOL.get('stock.location')
-        self.move = POOL.get('stock.move')
-        self.lot = POOL.get('stock.lot')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_lot_jreport')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
+    @with_transaction()
     def test0010get_lag_quantity(self):
         'Test get_lag_quantity'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+        pool = Pool()
+        StockConfiguration = pool.get('stock.configuration')
+        Uom = pool.get('product.uom')
+        Template = pool.get('product.template')
+        Product = pool.get('product.product')
+        Location = pool.get('stock.location')
+        Move = pool.get('stock.move')
+        Lot = pool.get('stock.lot')
 
-            supplier, = self.location.search([('code', '=', 'SUP')])
-            customer, = self.location.search([('code', '=', 'CUS')])
-            storage, = self.location.search([('code', '=', 'STO')])
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin'),
-                    ])
-            currency = company.currency
-            self.user.write([self.user(USER)], {
-                'main_company': company.id,
-                'company': company.id,
-                })
-            self.stock_configuration.create([{
+        # Create Company
+        company = create_company()
+        with set_company(company):
+
+            supplier, = Location.search([('code', '=', 'SUP')])
+            customer, = Location.search([('code', '=', 'CUS')])
+            storage, = Location.search([('code', '=', 'STO')])
+
+            StockConfiguration.create([{
                     'warehouse': storage.id,
                     'lag_days': 1,
                     }])
 
-            unit, = self.uom.search([('name', '=', 'Unit')])
-            template, = self.template.create([{
+            unit, = Uom.search([('name', '=', 'Unit')])
+            template, = Template.create([{
                         'name': 'Test period',
                         'type': 'goods',
                         'cost_price_method': 'fixed',
@@ -66,11 +51,11 @@ class TestCase(unittest.TestCase):
                         'list_price': Decimal(0),
                         'cost_price': Decimal(0),
                         }])
-            product, = self.product.create([{
+            product, = Product.create([{
                         'template': template.id,
                         }])
 
-            lot1, lot2 = self.lot.create([{
+            lot1, lot2 = Lot.create([{
                         'number': '1',
                         'product': product.id,
                         }, {
@@ -79,16 +64,14 @@ class TestCase(unittest.TestCase):
                         }])
 
             today = datetime.date.today()
-            moves = self.move.create([{
+            moves = Move.create([{
                         'product': product.id,
                         'lot': lot1.id,
                         'uom': unit.id,
                         'quantity': 5,
                         'from_location': supplier.id,
                         'to_location': storage.id,
-                        'company': company.id,
                         'unit_price': Decimal('1'),
-                        'currency': currency.id,
                         }, {
                         'product': product.id,
                         'lot': lot2.id,
@@ -96,9 +79,7 @@ class TestCase(unittest.TestCase):
                         'quantity': 10,
                         'from_location': supplier.id,
                         'to_location': storage.id,
-                        'company': company.id,
                         'unit_price': Decimal('1'),
-                        'currency': currency.id,
                         }, {
                         'product': product.id,
                         'lot': lot2.id,
@@ -106,9 +87,7 @@ class TestCase(unittest.TestCase):
                         'quantity': 2,
                         'from_location': storage.id,
                         'to_location': customer.id,
-                        'company': company.id,
                         'unit_price': Decimal('1'),
-                        'currency': currency.id,
                         }, {
                         'product': product.id,
                         'lot': lot1.id,
@@ -118,9 +97,7 @@ class TestCase(unittest.TestCase):
                         'to_location': storage.id,
                         'planned_date': today + relativedelta(days=2),
                         'effective_date': today + relativedelta(days=2),
-                        'company': company.id,
                         'unit_price': Decimal('1'),
-                        'currency': currency.id,
                         }, {
                         'product': product.id,
                         'lot': lot2.id,
@@ -130,27 +107,21 @@ class TestCase(unittest.TestCase):
                         'to_location': storage.id,
                         'planned_date': today - relativedelta(days=1),
                         'effective_date': today - relativedelta(days=1),
-                        'company': company.id,
                         'unit_price': Decimal('1'),
-                        'currency': currency.id,
                         }])
-            self.move.do(moves)
+            Move.do(moves)
 
             quantities = {
                 '1': 5,
                 '2': 18,
                 }
 
-            lots = self.lot.search([])
+            lots = Lot.search([])
             for lot in lots:
                 self.assertEqual(lot.lag_quantity, quantities[lot.number])
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite and not isinstance(test, doctest.DocTestCase):
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     return suite
